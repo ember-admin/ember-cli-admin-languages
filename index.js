e3fd6b07953041d9fd9bc2e593814f43116053f9
Igor Kur <kur91@mail.ru>
2015-01-12 16:20:25 +0200

    main blueprint

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-admin-languages'
+  name: 'ember-cli-admin-languages',
+
+  included: function(app) {
+    this._super.included(app);
+
+    app.import('bower_components/flag-icon-css/css/flag-icon.min.css');
+  }
+
 };
