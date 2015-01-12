6f12d65cd7c5c13b60fdfe11cd38ec2b934740fd
Igor Kur <kur91@mail.ru>
2015-01-12 17:58:48 +0200

    fix deps

--- blueprints/ember-cli-admin-languages/index.js
+++ blueprints/ember-cli-admin-languages/index.js
@@ -2,9 +2,7 @@ module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function() {
-    var self = this;
-    return this.addBowerPackageToProject('flag-icon-css').then(function(){
-      self.addBowerPackageToProject('bootstrap3-wysiwyg');
-    });
+    this.addBowerPackageToProject('flag-icon-css');
+    return this.addBowerPackageToProject('bootstrap3-wysiwyg');
   }
 };
