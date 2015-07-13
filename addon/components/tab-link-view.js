a42474cfed4925712f9155b59b057f63330c0168
thinkler <anslapidus@gmail.com>
2015-07-13 21:44:25 +0300

    Fixing components

---
+++ addon/components/tab-link-view.js
@@ -0,0 +1,29 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+  tabLinkView: Ember.Component.extend({
+    tagName: 'li',
+    classNameBindings: ['isActive:active:'],
+    attributeBindings: ['role'],
+
+    role: "presentation",
+
+    isActive: function(){
+      return (this.get('locales') || [])[0] === this.get('locale');
+    }.property('locale'),
+
+    href: function(){
+      return "#%@-%@".fmt(this.get('locale'), this.get('attribute'));
+    }.property('locale'),
+
+    localeClass: function(){
+      var locale = this.get('locale');
+      if(locale === 'en'){
+        locale = "gb";
+      }
+      return 'flag-icon flag-icon-%@'.fmt(locale);
+    }.property('locale')
+  })
+
+});
