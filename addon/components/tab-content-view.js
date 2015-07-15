60d120678ece510b1737c20748b2df405eea85ed
thinkler <anslapidus@gmail.com>
2015-07-15 09:36:28 +0300

    Refractor tab-conent-view component

--- addon/components/tab-content-view.js
+++ addon/components/tab-content-view.js
@@ -4,23 +4,23 @@ export default Ember.Component.extend({
   classNameBindings: ['isActive:active:'],
   attributeBindings: ['role', 'id'],
   classNames: ['tab-pane'],
-
   role: 'tabpanel',
 
+  getLocalName: function(reverse){
+    let locale = this.get('locale');
+    let attributeName = this.get('attributeName');
+    return (reverse) ? `${attributeName}_${locale}` : `${locale}-${attributeName}`;
+  },
 
   isActive: Ember.computed('locale', function(){
     return (this.get('locales') || [])[0] === this.get('locale');
   }),
 
   id: Ember.computed('locale', function(){
-    let locale = this.get('locale');
-    let attributeName = this.get('attributeName');
-    return `${locale}-${attributeName}`;
+    return this.getLocalName(false);
   }),
 
   attribute: Ember.computed('attributeName', 'locale', function(){
-    let locale = this.get('locale');
-    let attributeName = this.get('attributeName');
-    return `${attributeName}_${locale}`;
+    return this.getLocalName(true);
   })
 });
