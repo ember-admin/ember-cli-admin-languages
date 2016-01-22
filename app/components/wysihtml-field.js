b21f1bcc3b2700d96d2a07054ab584382ab6bce4
Oleg Mak <anslapidus@gmail.com>
2016-01-22 18:29:33 +0200

    Text area input fix

--- app/components/wysihtml-field.js
+++ app/components/wysihtml-field.js
@@ -20,7 +20,7 @@ export default Ember.Component.extend({
 
   value: Ember.computed('name', 'model', function(key, value) {
     if (arguments.length > 1) {
-      return this.get('model').set(this.get('name'), value);
+      this.get('model').set(this.get('name'), value);
     }
     return this.get('model').get(this.get('name'));
   })
